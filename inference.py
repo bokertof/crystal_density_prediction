d75571fc7433085af5aac8581ba040652f81be8d
Melnikov Stanislav <43165697+bokertof@users.noreply.github.com>
2021-06-01 16:13:49 +0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,6 +1,9 @@
-from random import choice
-from torchtext import data
-from functions import *
+from functions import load_config, load_dict
+from data_structures import infDENSDataset
+from model import RNN
+import torch
+from torch.utils.data import DataLoader
+
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -10,36 +13,33 @@ config = load_config("config.yaml")
 SMILES = load_dict(config["tokens_dict_name"])
 
 
-data_smiles = []
+
 with open(config["inference_data_name"],'r') as file:
 
-    for line in file:
-        data_smiles.append([[i.split(',') for i in line.strip().split(',,,')[:-1]], float(line.strip().split(',,,')[-1])])
+    data_smiles = [line.strip() for line in file.readlines()]
 
 
 
 
 
+input_size = embedd_size = len(SMILES.vocab.stoi.items())
 
-dataset_smiles = DENSDataset(data_smiles, SMILES)
+
+dataset_smiles = infDENSDataset(data_smiles, SMILES)
 dataloader = DataLoader(dataset_smiles, 
-                              batch_size=config["inference_BATCH_SIZE"],
-                              shuffle=False,
-                              collate_fn=dataset_smiles.collate_fn)
+                        batch_size=config["inference_BATCH_SIZE"],
+                        shuffle=False,
+                        collate_fn=dataset_smiles.collate_fn)
 
 
-
-embedd_size = len(SMILES.vocab.stoi.items())
-input_size = embedd_size
-
-
+     
+        
 aver_preds = torch.zeros(len(data_smiles), 1)
-
 for checkpoint in config["model_names"]:
     
     model = RNN(input_size, embedd_size, config["hidden_size"], config["num_layers"], config["bidirect"]).to(device)
     model.load_state_dict(torch.load(checkpoint)['state_dict'])
-    print(checkpoint, ' model successfuly loaded')
+    print(checkpoint.split('/')[-1], ' successfuly loaded')
 
     model.eval()
 
@@ -48,11 +48,7 @@ for checkpoint in config["model_names"]:
         index = 0
         with torch.no_grad():
             for batch in dataloader:
-                smiles = batch[0]
-                density = batch[1]
-
-                preds = model.forward(smiles.to(device)).to('cpu')
-
+                preds = model.forward(batch.to(device)).to('cpu')
                 aver_preds[index:index+preds.shape[0],:] += preds
 
                 index += preds.shape[0]
@@ -61,5 +57,6 @@ for checkpoint in config["model_names"]:
 aver_preds /= (len(config["model_names"])*(config["TTA_number"]))
 
 
-print(aver_preds)
 
+
+print(aver_preds.squeeze().tolist())
