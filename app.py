96e5a932c6cec99c93b605009be89c0dd6cacb9c
Stas Melnikov <43165697+bokertof@users.noreply.github.com>
2026-01-07 17:02:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -49,6 +49,7 @@ if st.button("Predict"):
                         index += preds.shape[0]
 
         aver_preds /= (len(models) * config["TTA_number"])
+        preds = aver_preds.view(-1).tolist()
 
-        for smi, pred in zip(data_smiles, aver_preds.squeeze().tolist()):
+        for smi, pred in zip(data_smiles, preds):
             st.write(f"**SMILES:** {smi} → **Predicted density:** {pred:.4f}")
